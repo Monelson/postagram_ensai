074111cd3552722d40094f05287be05121157033
Monelson <monelsonbaleus2023@gmail.com>
2026-04-26 23:43:55 +0200

    Open tofu update

--- open tofu/user_data.sh
+++ open tofu/user_data.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+exec > /var/log/user-data.log 2>&1
 echo "userdata-start"
-set -e
+set -eux
 
 apt update
 apt install -y python3-pip python3.12-venv git
+
+cd /home/ubuntu
 
 git clone ${git_repo} projet
 cd projet/webservice
 
 python3 -m venv venv
 source venv/bin/activate
-
-rm -f .env
 
 cat > .env <<EOF
 BUCKET=${bucket}
@@ -21,7 +22,12 @@ EOF
 export BUCKET="${bucket}"
 export DYNAMO_TABLE="${dynamo_table}"
 
+pip install --upgrade pip
 pip install -r requirements.txt
 
 nohup venv/bin/python app.py > app.log 2>&1 &
+
+sleep 5
+curl -I http://localhost:8080/docs || true
+
 echo "userdata-end"
